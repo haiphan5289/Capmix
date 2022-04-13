12415d71c26ef5a8591005065664ee9f1fa7a02e
haiphan5289 <haiphan5289@gmail.com>
2022-04-13 17:54:34 +0700

    finish recording

---
+++ Capmix/Capmix/CustomView/TimeLine/TimeLineView.swift
@@ -0,0 +1,23 @@
+//
+//  TimeLineView.swift
+//  Capmix
+//
+//  Created by haiphan on 13/04/2022.
+//
+
+import UIKit
+
+class TimeLineView: UIView {
+
+    @IBOutlet weak var lbTime: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    override func removeFromSuperview() {
+        superview?.removeFromSuperview()
+    }
+    
+}
--- Capmix/Capmix/Modules/Recording/RecordingVC.swift
+++ Capmix/Capmix/Modules/Recording/RecordingVC.swift
@@ -16,15 +16,11 @@ import EasyBaseAudio
 class RecordingVC: BaseVC {
     
     struct Constant {
-        static let offSetShadow: CGSize = CGSize(width: 0, height: -17)
-        static let opacityShadow: Float = 1
-        static let radiusShadow: CGFloat = 54
-        static let spaceMoveScroll: CGFloat = 4
-        static let mimiumMeter: CGFloat = 41
+        static let spaceMoveScroll: CGFloat = 6
         static let pixelWidth: CGFloat = 1
         static let pixelSpacing: CGFloat = 1
-        static let maximumHeightSound: CGFloat = 500
-        static let convertMilleToSecond: CGFloat = 10
+        static let withTimeLine: CGFloat = 60
+        static let miniMeter: CFloat = 50
     }
     
     enum Action {
@@ -37,6 +33,7 @@ class RecordingVC: BaseVC {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var centerView: UIView!
     @IBOutlet weak var vContentView: UIView!
+    @IBOutlet weak var timeLineStackView: UIStackView!
     
     // Add here your view model
     private var viewModel: RecordingVM = RecordingVM()
@@ -44,7 +41,7 @@ class RecordingVC: BaseVC {
     @VariableReplay private var status: Action = .prepare
     private var currentPosition: CGFloat = 0
     private var startPosition: CGFloat = 0
-    private var autoTime: Disposable?
+    private let valueMeter: PublishSubject<CFloat> = PublishSubject.init()
     
     private let disposeBag = DisposeBag()
     override func viewDidLoad() {
@@ -72,18 +69,34 @@ extension RecordingVC {
         // Add here the setup for the UI
         title = "Recording"
         self.recording = Recording(folderName: "\(ConstantApp.shared.folderRecording)")
+        self.recording.delegate = self
         do {
             try self.recording.prepare()
         } catch {
 
         }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            var f = self.timeLineStackView.frame
+            f.origin.y = 0
+            f.origin.x = self.positionXBetweenCenterAndScrollView()
+            self.timeLineStackView.frame = f
+        }
+        self.numberOfRecording(addSecond: 60)
     }
     
     private func setupRX() {
         // Add here the setup for the RX
         self.btPlay.rx.tap.bind { [weak self] _ in
             guard let wSelf = self else { return }
-            wSelf.status = .play
+            
+            if wSelf.btPlay.isSelected {
+                wSelf.status = .stop
+            } else {
+                wSelf.btPlay.isSelected = true
+                wSelf.status = .play
+            }
+            
         }.disposed(by: self.disposeBag)
         
         self.btStop.rx.tap.bind { [weak self] _ in
@@ -101,32 +114,37 @@ extension RecordingVC {
                 wSelf.btPlay.layer.borderWidth = 17
                 do {
                     try  wSelf.recording.record()
-                    wSelf.autoRunTimeMilieReplace()
                 } catch {
                     print(error.localizedDescription)
                 }
             case .stop:
                 wSelf.btPlay.layer.borderWidth = 17
                 wSelf.recording.stop()
-                wSelf.clearActionMilieReplace()
                 wSelf.navigationController?.popViewController(animated: true, nil)
-                print("======= \(wSelf.recording.url)")
+                print("====== Recording Finish =====")
+                print(wSelf.recording.url)
+                print("====== ******* =====")
             }
             
         }.disposed(by: self.disposeBag)
         
+        self.valueMeter.throttle(.milliseconds(100), scheduler: MainScheduler.asyncInstance).bind { [weak self] value in
+            guard let wSelf = self else { return }
+            let valueMeter = value + Constant.miniMeter
+            wSelf.autoScrollView()
+            wSelf.drawWaveView(height: valueMeter)
+        }.disposed(by: self.disposeBag)
+        
     }
     
-    private func drawWaveView(_ rect: CGRect, positionX: CGFloat, height: CGFloat) {
+    private func drawWaveView(height: Float) {
         let position = self.startPosition - self.positionXBetweenCenterAndScrollView() + self.startPosition
-        print("====== position \(position)")
-        print("====== height \(height)")
         let v: UIView = UIView(frame: .zero)
         v.backgroundColor = .red
         self.vContentView.addSubview(v)
         v.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
-            make.left.equalToSuperview().inset(positionX)
+            make.left.equalToSuperview().inset(position)
             make.height.equalTo(100)
             make.width.equalTo(Constant.pixelWidth)
         }
@@ -144,24 +162,27 @@ extension RecordingVC {
         return self.vContentView.convert(self.centerView.frame, to: nil).origin.x
     }
     
-    private func clearActionMilieReplace() {
-        autoTime?.dispose()
-    }
-    private func autoRunTimeMilieReplace() {
-        autoTime?.dispose()
-
-        autoTime = Observable<Int>.interval(.milliseconds(10), scheduler: MainScheduler.asyncInstance)
-            .bind(onNext: { [weak self] (time) in
-                guard let wSelf = self, let recording = wSelf.recording else { return }
-                if time % 10 == 0 {
-                    recording.recorder?.updateMeters()
-                    let ave = recording.recorder?.averagePower(forChannel: 0) ?? 0
-                    let peak = recording.recorder?.peakPower(forChannel: 0) ?? 0
-                    let h = (((CGFloat(ave) + Constant.mimiumMeter + 5) * wSelf.scrollView.frame.height) / Constant.maximumHeightSound)
-                    wSelf.autoScrollView()
-                    wSelf.drawWaveView(wSelf.vContentView.frame, positionX: wSelf.currentPosition, height: h)
-                }
-            })
+    private func numberOfRecording(addSecond: Int) {
+        for i in 0...addSecond {
+            self.addTimeLineView(index: i)
+        }
+        let count: CGFloat = CGFloat(self.timeLineStackView.subviews.count)
+        var f = self.timeLineStackView.frame
+        f.size.width = CGFloat(count * Constant.withTimeLine)
+        self.timeLineStackView.frame = f
     }
     
+    private func addTimeLineView(index: Int) {
+        let v: TimeLineView = TimeLineView.loadXib()
+        v.frame = CGRect(origin: .zero, size: CGSize(width: Constant.withTimeLine, height: 28))
+        v.lbTime.text = index.getTextFromSecond()
+        
+        self.timeLineStackView.addArrangedSubview(v)
+    }
+    
+}
+extension RecordingVC: RecorderDelegate {
+    func audioMeterDidUpdate(_ dB: Float) {
+        self.valueMeter.onNext(dB)
+    }
 }
